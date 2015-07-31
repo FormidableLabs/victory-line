180ec3cfd672283e61487e0aa684c35fe2179c94
Ross Donaldson <gastove@gmail.com>
2015-07-31 13:17:34 -0700

    AMAZE

--- src/components/victory-line.jsx
+++ src/components/victory-line.jsx
@@ -20,20 +20,23 @@ class VictoryLine extends React.Component {
         y: this.props.data.map(row => row.y)
       };
     } else {
-      this.state = {
-        x: this.returnOrGenerateX(),
-        y: this.returnOrGenerateY()
-      }
 
-      this.state.data = _.zip([this.state.x, this.state.y]);
+      this.state = {};
+      this.state.x = this.returnOrGenerateX();
+      this.state.y = this.returnOrGenerateY();
+
+      let inter = _.zip(this.state.x, this.state.y);
+      let objs = _.map(inter, (obj) => { return {x: obj[0], y: obj[1]}; });
+
+      this.state.data = objs;
     }
-
   }
 
   returnOrGenerateX() {
+    let step = Math.round(this.props.xMax / this.props.sample, 4);
     return this.props.x
          ? this.props.x
-         : _.range(this.props.xMin, this.props.xMax, this.props.sample)
+         : _.range(this.props.xMin, this.props.xMax, step)
   }
 
   returnOrGenerateY() {
@@ -41,7 +44,7 @@ class VictoryLine extends React.Component {
     if (typeof(y) === "array") {
       return y;
     } else if (typeof(y) === "function") {
-      return this.props.x.map(x => y(x))
+      return _.map(this.state.x, (x) => y(x))
     } else {
       // asplode
       return null;
@@ -67,8 +70,8 @@ class VictoryLine extends React.Component {
       svg: {
         "border": "2px solid black",
         "margin": "20px",
-        "width": "1000",
-        "height": "1000"
+        "width": "500",
+        "height": "200"
       }
     };
   }
@@ -76,23 +79,25 @@ class VictoryLine extends React.Component {
   render() {
     const styles = this.getStyles();
 
-    const xScale = this.props.scale(this.props.xMin, styles.width);
-    const yScale = this.props.scale(styles.height, this.props.yMin);
+    const xScale = this.props.scale(this.props.xMin, styles.svg.width);
+    const yScale = this.props.scale(styles.svg.height, this.props.yMin);
 
-    console.log(this.state.data);
+    let xExt = d3.extent(this.state.data, (obj) => obj.x);
+    let yExt = d3.extent(this.state.data, (obj) => obj.y);
 
-    xScale.domain(d3.extent(this.state.data, (data) => data.x));
-    yScale.domain(d3.extent(this.state.data, (data) => data.y));
+    xScale.domain(d3.extent(this.state.data, (obj) => obj.x));
+    yScale.domain(d3.extent(this.state.data, (obj) => obj.y));
 
     const d3Line = d3.svg.line()
-                     .x(obj => xScale(obj.x))
-                     .y(obj => yScale(obj.y));
+                     .x((obj) => xScale(obj.x))
+                     .y((obj) => yScale(obj.y));
 
-    const path = d3Line(data);
+    const path = d3Line(this.state.data);
 
     return (
-      <svg style={[styles.text]} >
-        <path d={path} />
+      <svg style={[styles.svg, this.props.style]} >
+        <path style={[styles.path, this.props.style]}
+              d={path} />
       </svg>
     );
   }
@@ -121,7 +126,7 @@ VictoryLine.defaultProps = {
   data: null,
   sample: 100,
   x: null,
-  y: () => Math.round(Math.random(), 1),
+  y: () => Math.random(),
   scale: (min, max) => d3.scale.linear().range([min, max])
 }
 
