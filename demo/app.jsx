21b6693594b4dfc51893afd4cda58cdecad58558
Ross Donaldson <gastove@gmail.com>
2015-07-30 17:01:28 -0700

    Delete test data

--- demo/app.jsx
+++ demo/app.jsx
@@ -12,15 +12,6 @@ class App extends React.Component {
   }
 }
 
-const data = [
-  {"x": 2, "y": 3},
-  {"x": 4, "y": 5},
-  {"x": 5, "y": 7},
-  {"x": 6, "y": 11},
-  {"x": 7, "y": 15},
-  {"x": 8, "y": 8},
-];
-
 const content = document.getElementById("content");
 
 React.render(<App />, content);
