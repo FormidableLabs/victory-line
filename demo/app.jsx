23e28c70770ca06764938795621567b18a33dc5b
Lauren <lauren.eastridge@formidablelabs.com>
2015-08-13 15:29:18 -0700

    refactor line

--- demo/app.jsx
+++ demo/app.jsx
@@ -4,26 +4,42 @@ import {VictoryLine} from "../src/index";
 
 class App extends React.Component {
   render() {
+    const style = {
+      border: "2px solid black",
+      margin: 5,
+      width: 500,
+      height: 200
+    }
+
     return (
       <div className="demo">
-        <VictoryLine style={{stroke: "blue"}}
-                     y={(x) => Math.sin(x)}
-                     sample={25}/>
-        <VictoryLine style={{stroke: "green"}}
-                     y={(x) => x * x} />
-        <VictoryLine />
-        <VictoryLine data={[
-                           {x: 1, y: 1},
-                           {x: 2, y: 4},
-                           {x: 3, y: 5},
-                           {x: 4, y: 2},
-                           {x: 5, y: 11},
-                           {x: 6, y: 7},
-                           {x: 7, y: 6},
-                           {x: 8, y: 7},
-                           {x: 9, y: 8},
-                           {x: 10, y: 12}
-                           ]}/>
+        <svg style={style}>
+          <VictoryLine style={[style, {stroke: "blue"}]}
+            y={(x) => Math.sin(x)}
+            sample={25}/>
+        </svg>
+        <svg style={style}>
+          <VictoryLine style={[style, {stroke: "green"}]}
+            y={(x) => x * x} />
+        </svg>
+        <svg style={style}>
+          <VictoryLine />
+        </svg>
+        <svg style={style}>
+          <VictoryLine
+            data={[
+              {x: 1, y: 1},
+              {x: 2, y: 4},
+              {x: 3, y: 5},
+              {x: 4, y: 2},
+              {x: 5, y: 11},
+              {x: 6, y: 7},
+              {x: 7, y: 6},
+              {x: 8, y: 7},
+              {x: 9, y: 8},
+              {x: 10, y: 12}
+            ]}/>
+        </svg>
       </div>
     );
   }
--- src/components/victory-line.jsx
+++ src/components/victory-line.jsx
@@ -5,115 +5,150 @@ import _ from "lodash";
 
 @Radium
 class VictoryLine extends React.Component {
-
   constructor(props) {
     super(props);
-
-    // Initialize state.
     this.state = {};
+  }
 
-    const styles = this.getStyles();
+  getStyles() {
+    return _.merge({
+      path: {
+        fill: "none",
+        stroke: "darkgrey",
+        strokeWidth: 2
+      },
+      svg: {
+        border: "2px solid black",
+        margin: 5,
+        width: 500,
+        height: 200
+      }
+    }, this.props.style);
+  }
 
-    // Extrapolate x and y range from style
-    this.state.xRange = {
-      min: styles.svg.margin,
-      max: styles.svg.width - styles.svg.margin
-    };
-    this.state.yRange = {
-      min: styles.svg.margin,
-      max: styles.svg.height - styles.svg.margin
-    };
+  getScale(type) {
+    const scale = this.props.scale[type] ? this.props.scale[type]().copy() :
+      this.props.scale().copy();
+    const range = this.getRange(type);
+    const domain = this.getDomain(type);
+    scale.range(range);
+    scale.domain(domain);
+    // hacky check for identity scale
+    if (_.difference(scale.range(), range).length !== 0) {
+      // identity scale, reset the domain and range
+      scale.range(range);
+      scale.domain(range);
+      this.warn("Identity Scale: domain and range must be identical. " +
+        "Domain has been reset to match range.");
+    }
+    return scale;
+  }
 
+  getDomain(type) {
+    // scale is never undefined thanks to defaults
+    const scaleDomain = this.props.scale[type] ? this.props.scale[type]().domain() :
+      this.props.scale().domain();
+    let domain;
+    if (this.props.domain) {
+      domain = this.props.domain[type] ? this.props.domain[type] : this.props.domain;
+    } else if (this.props.data) {
+      domain = [_.min(this.props.data, type), _.max(this.props.data, type)];
+    } else if (this.props[type]) {
+      domain = [_.min(this.props[type]), _.max(this.props[type])];
+      else if (this.props.scale)
+      domain = scaleDomain;
+    }
+    // Warn when domains need more information to produce meaningful axes
+    if (domain === scaleDomain && _.isDate(scaleDomain[0])) {
+      log.warn("please specify tickValues or domain when creating a time scale axis");
+    } else if (domain === scaleDomain && scaleDomain.length === 0) {
+      log.warn("please specify tickValues or domain when creating an axis using " +
+        "ordinal or quantile scales");
+    } else if (domain === scaleDomain && scaleDomain.length === 1) {
+      log.warn("please specify tickValues or domain when creating an axis using " +
+        "a threshold scale");
+    }
+    return domain;
+  }
+
+  getRange(type) {
+    if (this.props.range) {
+      return this.props.range[type] ? this.props.range[type] : this.props.range;
+    }
+    const style = this.getStyles().svg; // TODO: hacky
+    const dimension = type === "x" ? "width" : "height"
+    return [style.margin, style[dimension] - style.width];
+  }
+
+  returnOrGenerateX() {
+    if (this.props.x) {
+      return this.props.x;
+    }
+    const domain = this.getDomain("x");
+    const samples = _.isArray(this.props.y) ? this.props.y.length : this.props.sample;
+    const step = _.round(_.max(domain) / samples, 2);
+    return _.range(_.min(domain), _.max(domain), step);
+  }
+
+  returnOrGenerateY() {
+    const y = this.props.y;
+    const x = this.returnOrGenerateX();
+    if (typeof y === "object" && y.isArray()) {
+      return y;
+    } else if (typeof y === "function") {
+      return _.map(x, (x) => y(x));
+    } else {
+      // asplode
+      return [];
+    }
+  }
+
+  getData() {
     /*
        Our use-cases are:
        1. The user passes in data as an array of {x: 1, y: 2}
-       2. The user provides no x; make it from xMin and xMax
+       2. The user provides no x; make it from the domain
        3. The user provides x as an array of points; leave it be
        4. The user provides y as an array of points; leave it be
        5. The user provides y as a function; use x to generate y
      */
 
     if (this.props.data) {
-      this.state.data = this.props.data;
-      this.state.x = this.props.data.map(row => row.x);
-      this.state.y = this.props.data.map(row => row.y);
-    } else {
-      this.state.x = this.returnOrGenerateX();
-      this.state.y = this.returnOrGenerateY();
-
-      const inter = _.zip(this.state.x, this.state.y);
-      const objs = _.map(inter, (obj) => { return {x: obj[0], y: obj[1]}; });
-
-      this.state.data = objs;
+      return this.props.data;
     }
+    const x = this.returnOrGenerateX();
+    const y = this.returnOrGenerateY();
+    const n = _.min([x.length, y.length]);
+    // create a dataset from x and y with n points
+    const dataset = _.zip(_.take(x, n), _.take(y, n));
+    // return data as an array of objects
+    return _.map(dataset, (point) => {
+      return {x: point[0], y: point[1]};
+    });
   }
 
-  returnOrGenerateX() {
-    const step = Math.round(this.state.xRange.max / this.props.sample, 4);
-    return this.props.x
-         ? this.props.x
-         : _.range(this.state.xRange.min, this.state.xRange.max, step);
-  }
+  drawLine() {
+    const xScale = this.getScale("x");
+    const yScale = this.getScale("y");
 
-  returnOrGenerateY() {
-    const y = this.props.y;
-    if (typeof y === "object" && y.isArray()) {
-      return y;
-    } else if (typeof y === "function") {
-      return _.map(this.state.x, (x) => y(x));
-    } else {
-      // asplode
-      return null;
-    }
-  }
+    const d3Line = d3.svg.line()
+      .interpolate(this.props.interpolation)
+      .x((data) => xScale(data.x))
+      .y((data) => yScale(data.y));
 
-  getStyles() {
-    return _.merge({
-      base: {
-        color: "#000",
-        fontSize: 12,
-        textDecoration: "underline"
-      },
-      red: {
-        color: "#d71920",
-        fontSize: 30
-      },
-      path: {
-        "fill": "none",
-        "stroke": "darkgrey",
-        "strokeWidth": "2px"
-      },
-      svg: {
-        "border": "2px solid black",
-        "margin": "5",
-        "width": "500",
-        "height": "200"
-      }
-    }, this.props.style);
+    const path = d3Line(this.getData());
+    const style = this.getStyles();
+    return (
+      <path style={[style.path, this.props.style]}
+          d={path} />
+    );
   }
 
   render() {
-    const styles = this.getStyles();
-
-    const xScale = this.props.scale(this.state.xRange.min, this.state.xRange.max);
-    const yScale = this.props.scale(this.state.yRange.max, this.state.yRange.min);
-
-    xScale.domain(d3.extent(this.state.data, (obj) => obj.x));
-    yScale.domain(d3.extent(this.state.data, (obj) => obj.y));
-
-    const d3Line = d3.svg.line()
-                     .interpolate(this.props.interpolation)
-                     .x((obj) => xScale(obj.x))
-                     .y((obj) => yScale(obj.y));
-
-
-    const path = d3Line(this.state.data);
-
     return (
-      <svg style={[styles.svg, this.props.style]} >
-        <path style={[styles.path, this.props.style]}
-              d={path} />
-      </svg>
+      <g>
+        {this.drawLine}
+      <g>
     );
   }
 }
@@ -125,6 +160,40 @@ VictoryLine.propTypes = {
       y: React.PropTypes.number
     })
   ),
+  x: React.PropTypes.array,
+  y: React.PropTypes.oneOfType([
+    React.PropTypes.array,
+    React.PropTypes.func
+  ]),
+  domain: React.PropTypes.oneOfType([
+    React.PropTypes.array,
+    React.PropTypes.objectOf(
+      React.PropTypes.shape({
+        x: React.PropTypes.array,
+        y: React.PropTypes.array
+      })
+    )
+  ]),
+  range: React.PropTypes.oneOfType([
+    React.PropTypes.array,
+    React.PropTypes.objectOf(
+      React.PropTypes.shape({
+        x: React.PropTypes.array,
+        y: React.PropTypes.array
+      })
+    )
+  ]),
+  scale: React.PropTypes.oneOfType([
+    React.PropTypes.func,
+    React.PropTypes.objectOf(
+      React.PropTypes.shape({
+        x: React.PropTypes.func,
+        y: React.PropTypes.func
+      })
+    )
+  ]),
+  style: React.PropTypes.node,
+  sample: React.PropTypes.number,
   interpolation: React.PropTypes.oneOf([
     "linear",
     "linear-closed",
@@ -139,25 +208,15 @@ VictoryLine.propTypes = {
     "cardinal-open",
     "cardinal-closed",
     "monotone"
-  ]),
-  sample: React.PropTypes.number,
-  scale: React.PropTypes.func,
-  style: React.PropTypes.node,
-  x: React.PropTypes.array,
-  y: React.PropTypes.oneOfType([
-    React.PropTypes.array,
-    React.PropTypes.func
   ])
 };
 
 VictoryLine.defaultProps = {
-  data: null,
   interpolation: "basis",
-  range: [0, 100],
   sample: 100,
-  scale: (min, max) => d3.scale.linear().range([min, max]),
-  x: null,
-  y: () => Math.random()
+  scale: () => d3.scale.linear(),
+  y: () => Math.random(),
+  domain: [0, 100]
 };
 
 export default VictoryLine;
